a6466dabd072e509f425b154e263acb5f03d52a7
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-11-09 09:52:10 +0100

    structs

--- src/main.c
+++ src/main.c
@@ -6,6 +6,42 @@
 
 char srcfile[2048];
 char first = 1;
+
+size_t getTypeArray(CXCursor cursor, char** type, const char** array) // does malloc so.. cleanup after
+{
+    size_t len = 0;
+
+    if(clang_getNumElements(clang_getCursorType(cursor)) != -1)
+    {
+        len = clang_getNumElements(clang_getCursorType(cursor));
+    }
+
+    const char* typename = clang_getCString(clang_getTypeSpelling(clang_getCursorType(cursor)));
+    size_t nlen = strlen(typename) + 1;
+    char* modname = malloc(nlen);
+    memcpy(modname, typename, nlen);
+
+    for(int i = 1; i < nlen - 1; i++)
+    {
+        if(modname[i] == '[')
+        {
+            typename += i;
+            modname[i] = 0;
+
+            if(modname[i - 1] == ' ')
+            {
+                modname[i - 1] = 0;
+            }
+
+            break;
+        }
+    }
+
+    *array = typename;
+    *type = modname;
+    return len;
+}
+
 // get globals
 enum CXChildVisitResult printchildglobals(CXCursor cursor, CXCursor parent, CXClientData client_data)
 {
@@ -18,35 +54,12 @@ enum CXChildVisitResult printchildglobals(CXCursor cursor, CXCursor parent, CXCl
         clang_getPresumedLocation(loc, &filename, &line, &col);
         const char* f = clang_getCString(filename);
         const char* p = clang_getCString(clang_getCursorSpelling(parent));
-        size_t len = 0;
 
         if(strcmp(f, srcfile) == 0 && strcmp(p, srcfile) == 0)
         {
-            if(clang_getNumElements(clang_getCursorType(cursor)) != -1)
-            {
-                len = clang_getNumElements(clang_getCursorType(cursor));
-            }
-
-            const char* typename = clang_getCString(clang_getTypeSpelling(clang_getCursorType(cursor)));
-            size_t nlen = strlen(typename) + 1;
-            char* modname = malloc(nlen);
-            memcpy(modname, typename, nlen);
-
-            for(int i = 1; i < nlen - 1; i++)
-            {
-                if(modname[i] == '[')
-                {
-                    typename += i;
-                    modname[i] = 0;
-
-                    if(modname[i - 1] == ' ')
-                    {
-                        modname[i - 1] = 0;
-                    }
-
-                    break;
-                }
-            }
+            char* type = 0;
+            const char* array = 0;
+            size_t len = getTypeArray(cursor, &type, &array);
 
             if(first)
             {
@@ -57,18 +70,115 @@ enum CXChildVisitResult printchildglobals(CXCursor cursor, CXCursor parent, CXCl
                 printf(",\n");
             }
 
-            printf("{\"type\":\"%s\",", modname);
+            printf("{\"type\":\"%s\",", type);
 
             if(len != 0)
             {
-                printf("\"array\":\"%s\",", typename);
+                printf("\"array\":\"%s\",", array);
             }
 
             printf("\"name\":\"%s\",", clang_getCString(clang_getCursorSpelling(cursor)));
             printf("\"src\":\"%s\",\"line\":\"%i\",\"col\":\"%i\"}",
                    clang_getCString(filename), line, col
                   );
-            free(modname);
+            free(type);
+        }
+    }
+
+    return CXChildVisit_Recurse;
+}
+
+char firststructvar = 0;
+int svarorder = 0;
+enum CXChildVisitResult printchildstructs_vars(CXCursor cursor, CXCursor parent, CXClientData client_data)
+{
+    if(clang_getCursorKind(cursor) == CXCursor_FieldDecl)
+    {
+        if(firststructvar)
+        {
+            firststructvar = 0;
+            svarorder = 0;
+        }
+        else
+        {
+            svarorder++;
+            printf(",");
+        }
+
+        char* type = 0;
+        const char* array = 0;
+        size_t len = getTypeArray(cursor, &type, &array);
+        printf("{\"name\":\"%s\",\"order\":\"%i\",\"type\":\"%s\"",
+               clang_getCString(clang_getCursorSpelling(cursor)),
+               svarorder,
+               type
+              );
+
+        if(len != 0)
+        {
+            printf(",\"array\":\"%s\"", array);
+        }
+
+        printf("}");
+        free(type);
+        /*fprintf(stderr, "\n\tkind:%s parent:%s name:%s",*/
+        /*clang_getCString(clang_getCursorKindSpelling(clang_getCursorKind(cursor))),*/
+        /*clang_getCString(clang_getCursorKindSpelling(clang_getCursorKind(parent))),*/
+        /*clang_getCString(clang_getCursorSpelling(cursor))*/
+        /*);*/
+    }
+
+    return CXChildVisit_Continue;
+}
+// get structs
+enum CXChildVisitResult printchildstructs(CXCursor cursor, CXCursor parent, CXClientData client_data)
+{
+    if(clang_getCursorKind(cursor) == CXCursor_StructDecl)
+    {
+        /*fprintf(stderr, "kind:%s parent:%s name:%s\n",*/
+        /*clang_getCString(clang_getCursorKindSpelling(clang_getCursorKind(cursor))),*/
+        /*clang_getCString(clang_getCursorKindSpelling(clang_getCursorKind(parent))),*/
+        /*clang_getCString(clang_getCursorSpelling(cursor))*/
+        /*);*/
+        CXSourceLocation loc = clang_getCursorLocation(cursor);
+        CXString filename;
+        unsigned int line;
+        unsigned int col;
+        clang_getPresumedLocation(loc, &filename, &line, &col);
+        const char* f = clang_getCString(filename);
+        const char* p = clang_getCString(clang_getCursorSpelling(parent));
+
+        if(strcmp(f, srcfile) == 0 && strcmp(p, srcfile) == 0)
+        {
+            char* type = 0;
+            const char* array = 0;
+            size_t len = getTypeArray(cursor, &type, &array);
+
+            if(first)
+            {
+                first = 0;
+            }
+            else
+            {
+                printf(",\n");
+            }
+
+            printf("{");
+
+            if(len != 0)
+            {
+                printf("\"array\":\"%s\",", array);
+            }
+
+            printf("\"name\":\"%s\",", clang_getCString(clang_getCursorSpelling(cursor)));
+            printf("\"src\":\"%s\",\"line\":\"%i\",\"col\":\"%i\",",
+                   clang_getCString(filename), line, col
+                  );
+            printf("\"variables\":[\n");
+            firststructvar = 1;
+            clang_visitChildren(cursor, printchildstructs_vars, 0);
+            printf("]}");
+            free(type);
         }
     }
 
@@ -115,11 +225,15 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 
-    int level = 0;
     printf("{\"globals\": [");
     CXCursor cursor = clang_getTranslationUnitCursor(unit);
     first = 1;
     clang_visitChildren(cursor, printchildglobals, 0);
+    printf("],\n");
+    printf("\"structs\": [");
+    cursor = clang_getTranslationUnitCursor(unit);
+    first = 1;
+    clang_visitChildren(cursor, printchildstructs, 0);
     printf("]}\n");
     clang_disposeTranslationUnit(unit);
     clang_disposeIndex(index);
